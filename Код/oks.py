b50a97ebe836573f0095c1d07941ca9fd87722cd
ibdch <vaghina2001@mail.ru>
2022-03-18 00:10:06 +0600

    Update oks.py

--- Код/oks.py
+++ Код/oks.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
 
 # подготовка данных
 data = pd.read_csv('oks.csv',sep=r'\s*,\s*', header=0, encoding='utf8', engine='python')
@@ -13,7 +12,4 @@ plt.title('Динамика изменения цен товара')
 # Используем Matplotlib, чтобы нарисовать линейную диаграмму
 plt.plot(x, y)
 plt.show()
-# Используем Seaborn, чтобы нарисовать линейный график
-df = pd.DataFrame({'x': x, 'y': y})
-sns.lineplot(x="x", y="y", data=df)
-plt.show()
+
