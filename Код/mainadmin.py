c2f065e8883fc2246a95d9cff03600f0373e8606
ibdch <vaghina2001@mail.ru>
2022-03-17 23:43:25 +0600

    Update mainadmin.py

--- Код/mainadmin.py
+++ Код/mainadmin.py
@@ -6,6 +6,8 @@ def gonna_reg_click():
     window.destroy()
     import wb
 
+def gonna_tov():
+    import oks
 
 window = tk.Tk()
 window.title('WILDBERRIES')
@@ -18,13 +20,17 @@ main_menu.add_cascade(label='Справка', menu=file_menu)
 
 menu = Menu(window)
 submenu = Menu(window)
-window.config(menu=menu,bg='#7B68EE')
+submenu2 = Menu(window)
+window.config(menu=menu,bg='white')
 submenu.add_command(label="Товары с наибольшим спросом", command=gonna_reg_click)
-submenu.add_command(label="Динамика смены цен товара", command=gonna_reg_click)
+submenu.add_cascade(label="Динамика смены цен товара", menu=submenu2)
 submenu.add_command(label="Статистика улиц проживания абонентов", command=gonna_reg_click)
 submenu.add_command(label="Стоимость товара по декадам", command=gonna_reg_click)
 menu.add_cascade(label="Обработка данных", menu=submenu)
-
+submenu2.add_command(label="Кепка Puma", command=gonna_tov)
+submenu2.add_command(label="Кепка North Face", command=gonna_tov)
+submenu2.add_command(label="Кепка Nike", command=gonna_tov)
+submenu2.add_command(label="Кепка KINZA", command=gonna_tov)
 
 file_menu2 = Menu()
 menu.add_cascade(label="Справка", menu=file_menu2)
@@ -37,11 +43,9 @@ file_menu3.add_command(label="Светлая")
 file_menu3.add_command(label="Темная")
 
 
-poetry = "ДОБРО ПОЖАЛОВАТЬ \n НА WILDBERRIES"
-label2 = Label(text=poetry, justify=CENTER, bg="#333", fg="#eee", font='Arial 24')
-label2.place(relx=.2, rely=.3)
+poetry = "ДОБРО ПОЖАЛОВАТЬ \nНА СТРАНИЦУ АДМИНИСТРАТОРА \nWILDBERRIES"
+label2 = Label(text=poetry, justify=CENTER, bg="white", fg="black", font='Arial 24')
+label2.place(relx=.04, rely=.3)
 
-author_label = tk.Label(window, text='Авторы: Вагина О., Серикова Д., Бурханов Р., Кушманов Е.', bg='#7B68EE')
-author_label.place(relx=0, rely=1, anchor='sw')
 
 window.mainloop()
